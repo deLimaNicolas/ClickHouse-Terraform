420efbd7098b0de7f86aef71cd683f6f0da9472d
Nicolas Lma <teixlima@gmail.com>
2020-01-21 06:43:07 -0300

    feature/ssh allowed *

--- src/terraform/clickhouse-server/main.tf
+++ src/terraform/clickhouse-server/main.tf
@@ -5,10 +5,30 @@ resource "aws_instance" "default" {
   key_name                = var.key-pair
   private_ip              = var.private-ip
   subnet_id               = var.subnet-id
-  vpc_security_group_ids = var.vpc-security-group-ids
+  security_groups         = ["${aws_security_group.allow_ssh.name}"]
   
   tags = {
   Name = var.name
 }
 
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name = "allow_ssh"
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "public_dns" {
+  value = "${aws_instance.default.public_dns}"
 }
